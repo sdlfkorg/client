e8742bdceba7641bf4dfb7129c7e9ab6d7856283
sdlfkorg <sdlfkorg@gmail.com>
2018-07-29 16:35:26 +0800

    including redux and create redux store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
+import reducers from './reducers';
 import App from './components/App';
 import Message from './components/Message';
 import Signup from './components/auth/Signup';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App>
-			<Route path="/" exact component={Message} />
-			<Route path="/signup" exact component={Signup} />
-		</App>
-	</BrowserRouter>,
+	<Provider store={createStore(reducers, {})}>
+		<BrowserRouter>
+			<App>
+				<Route path="/" exact component={Message} />
+				<Route path="/signup" exact component={Signup} />
+			</App>
+		</BrowserRouter>
+	</Provider>,
 	document.querySelector('#root')
 );
