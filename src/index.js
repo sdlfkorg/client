f50dbb435dab6e9db8deea17d66a5bd9dbacd141
sdlfkorg <sdlfkorg@gmail.com>
2018-07-29 16:06:38 +0800

    components: add signup form

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/App';
 import Message from './components/Message';
+import Signup from './components/auth/Signup';
 
 ReactDOM.render(
 	<BrowserRouter>
 		<App>
 			<Route path="/" exact component={Message} />
+			<Route path="/signup" exact component={Signup} />
 		</App>
 	</BrowserRouter>,
 	document.querySelector('#root')
