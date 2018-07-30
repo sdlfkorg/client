329f8e7e7dd51e742737bd70581d8d33ed497de7
sdlfkorg <sdlfkorg@gmail.com>
2018-07-30 17:07:53 +0800

    signup feature: wire up the backend, action and reducer, and get the jwt

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-import AUTH_USER from './type';
+import {AUTH_USER} from './type';
 
-export const signup = formProps => dispatch => {
-	axios.post('http://localhost:3090/signup', formProps);
+export const signup = formProps => async dispatch => {
+	const response = await axios.post('http://localhost:3090/signup', formProps);
+
+	console.log(response);
+	dispatch({type: AUTH_USER, payload: response.data.token})
 };
 	
 // formProps = {email, password}
