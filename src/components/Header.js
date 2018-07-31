04f98a1d89135ddc616bf7990ea559f206be6587
sdlfkorg <sdlfkorg@gmail.com>
2018-07-31 17:43:18 +0800

    header components: add show and hide logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 class Header extends Component {
+	renderLinks(){
+		if (this.props.authenticated){
+			return (
+				<div>
+					<Link to="/signout">Sign Out</Link>
+					<Link to="/feature">Feature</Link>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<Link to="/signup">Sign Up</Link>
+					<Link to="/signin">Sign In</Link>
+				</div>
+			);
+		}
+
+	}
 	render(){
 		return (
 			<div>
 				<Link to="/">Home</Link>
-				<Link to="/signup">Sign Up</Link>
-				<Link to="/signin">Sign In</Link>
-				<Link to="/signout">Sign Out</Link>
-				<Link to="/feature">Feature</Link>
+				{this.renderLinks()}
 
 			</div>
 		);
 	}
 }
 
-export default Header;
+function mapStateToProps(state){
+	return {authenticated: state.auth.authenticated};
+}
+
+export default connect(mapStateToProps)(Header);
